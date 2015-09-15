4cc044a7eae05ccef55a1bbfa15bc4afa799350f
Nico Hoffmann <github@ohnehin.com>
2015-09-15 16:05:28 +0200

    More stable font paths

--- core/assets.php
+++ core/assets.php
@@ -9,8 +9,8 @@ class Assets {
 
   public static function css($hook = null) {
     $style  = tpl::load(realpath(__DIR__ . '/..') . DS . 'assets' . DS . 'css' . DS . 'panelbar.min.css');
-    $script .= self::hook($hook);
-    return '<style>'.$style.'</style>';
+    $style .= self::hook($hook);
+    return '<style>'.self::paths($style).'</style>';
   }
 
   public static function js($hook = null) {
@@ -20,6 +20,18 @@ class Assets {
     $script .= tpl::load(realpath(__DIR__ . '/..') . DS . 'assets' . DS . 'js' . DS . 'panelbar.min.js');
     $script .= self::hook($hook);
     return '<script>'.$script.'</script>';
+  }
+
+  protected static function paths($code){
+    $base = str_repeat('../', substr_count(str_ireplace(kirby()->roots()->index(),'',__DIR__), '/'));
+    $fonts = $base . 'panel/assets/fonts/';
+
+    // Fonts
+    $code = str_ireplace('{{FA}}', $fonts.'fontawesome-webfont.woff?v=4.2.0', $code);
+    $code = str_ireplace('{{SSP400}}', $fonts.'sourcesanspro-400.woff', $code);
+    $code = str_ireplace('{{SSP600}}', $fonts.'sourcesanspro-600.woff', $code);
+    $code = str_ireplace('{{SSPitalic}}', $fonts.'sourcesanspro-400-italic.woff', $code);
+    return $code;
   }
 
   protected static function hook($hook)  {
