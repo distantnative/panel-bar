82c704973a5973f430ddccf68e0c04724ff4e97f
Nico Hoffmann <github@ohnehin.com>
2015-10-02 14:30:45 +0200

    Reload iFrame only if necessary, fix #75

--- assets/js/elements/iframe.js
+++ assets/js/elements/iframe.js
@@ -11,6 +11,8 @@ var PanelbarIframe = function() {
   this.panelbar  = document.getElementById("panelbar");
   this.elements  = document.querySelectorAll(".panelbar__bar > div");
   this.position  = null;
+  this.form      = null;
+  this.submitted = false;
 
 
   this.activate = function(link) {
@@ -23,16 +25,18 @@ var PanelbarIframe = function() {
     self.clearPosition();
 
     self.load();
+    setTimeout(self.bindSubmit, 500);
   };
 
   this.deactivate = function() {
-    if (true) {
+    if (self.submitted) {
+      location.reload();
+
+    } else {
       self.restorePanelbar();
       self.removeOverlay();
       self.restorePosition();
-
-    } else {
-      location.reload();
+      self.unbindSubmit();
     }
   };
 
@@ -40,19 +44,34 @@ var PanelbarIframe = function() {
     self.iframe.src = self.href;
   };
 
+  this.bindSubmit = function() {
+    self.clearSubmit();
+
+    var inner     = self.iframe.contentDocument || self.iframe.contentWindow.document;
+    self.form     = inner.querySelector('.form');
+    self.form.addEventListener('submit', self.setSubmit);
+  };
+
+  this.unbindSubmit = function() {
+    self.form.removeEventListener('submit', self.setSubmit);
+  };
+
+  this.setSubmit   = function() { self.submitted = true; }
+  this.clearSubmit = function() { self.submitted = false; }
+
 
   this.buildOverlay = function() {
-    self.returnBtn.style.display = "inline-block";
-    self.wrapper.style.display   = "block";
+    self.returnBtn.style.display = 'inline-block';
+    self.wrapper.style.display   = 'block';
     document.body.style.overflow = 'hidden';
 
     self.returnBtn.addEventListener('click', self.deactivate);
   };
 
   this.removeOverlay = function() {
-    self.returnBtn.style.display = "none";
-    self.wrapper.style.display   = "none";
-    document.body.style.overflow = "auto";
+    self.returnBtn.style.display = 'none';
+    self.wrapper.style.display   = 'none';
+    document.body.style.overflow = 'auto';
 
     self.returnBtn.removeEventListener('click', self.deactivate);
   };
