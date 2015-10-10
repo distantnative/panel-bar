e5ee65cb682a1b87d637d42700ded4d6728efdf9
Nico Hoffmann <github@ohnehin.com>
2015-10-10 14:31:21 +0200

    Small iFrame btn CSS fix

--- templates/iframe/btn.php
+++ templates/iframe/btn.php
@@ -1,8 +1,8 @@
 <span class="panelbar-iframe__btns">
   <span class="panelbar-element panelbar-btn js_panelbar-iframe-close">
-    <a><i class="fa fa-arrow-circle-left"></i> Return to page</a>
+    <a><i class="fa fa-arrow-circle-left"></i> <span>Return to Page</span></a>
   </span>
   <span class="panelbar-element panelbar-btn js_panelbar-iframe-closerefresh">
-    <a><i class="fa fa-refresh"></i> Return and Refresh</a>
+    <a><i class="fa fa-refresh"></i> <span>Return and Refresh</span></a>
   </span>
 </span>
