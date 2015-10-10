1fd18d9e4ba99a7b544595e1f9b1f53bddc17017
Nico Hoffmann <github@ohnehin.com>
2015-10-10 20:57:32 +0200

    Title tags for Fileviewer items

--- templates/elements/fileviewer.php
+++ templates/elements/fileviewer.php
@@ -2,23 +2,16 @@
 
   <div class="panelbar-fileviewer__items">
     <?php foreach($items as $item) : ?>
-      <a href="<?php echo $item['url'] ?>" class="panelbar-fileviewer__item panelbar-fileviewer__item--<?php echo $item['type'] ?>">
+      <a href="<?php echo $item['url'] ?>" class="panelbar-fileviewer__item panelbar-fileviewer__item--<?php echo $item['type'] ?>" title="<?php echo $item['label'] ?>">
         <div class="panelbar-fileviewer__preview">
-
           <?php if ($item['type'] == 'image') : ?>
             <div class="panelbar-fileviewer__image" style="background-image:url('<?php echo $item['image'] ?>');"></div>
           <?php endif ?>
 
-          <em>
-            <?php echo $item['extension'] ?>
-          </em>
-
-          <span class="panelbar-fileviewer__label">
-            <?php echo $item['label'] ?>
-          </span>
-
+          <div class="panelbar-fileviewer__overlay"></div>
+          <em><?php echo $item['extension'] ?></em>
+          <span class="panelbar-fileviewer__label"><?php echo $item['label'] ?></span>
         </div>
-
       </a>
     <?php endforeach ?>
   </div>
@@ -26,5 +19,4 @@
   <a href="<?php echo $all['url'] ?>" class="panelbar-fileviewer__more">
     All <?php echo $all['label'] ?>
   </a>
-
 </div>
