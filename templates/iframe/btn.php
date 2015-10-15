f68f6d0a6ba8e09110e774e49f868aac312d0d1d
Nico Hoffmann <github@ohnehin.com>
2015-10-15 16:21:56 +0200

    Fix #112

--- templates/iframe/btn.php
+++ templates/iframe/btn.php
@@ -1,8 +1,8 @@
 <span class="panelbar-iframe__btns">
   <span class="panelbar-element panelbar-btn js_panelbar-iframe-close" title="Alt + E">
-    <a><i class="fa fa-arrow-circle-left"></i> <span>Return to Page</span></a>
+    <a><i class="fa fa-arrow-circle-left"></i> <span class="not-mobile">Return to Page</span></a>
   </span>
   <span class="panelbar-element panelbar-btn js_panelbar-iframe-closerefresh" title="Alt + R">
-    <a><i class="fa fa-refresh"></i> <span>Return and Refresh</span></a>
+    <a><i class="fa fa-refresh"></i> <span class="not-mobile">Return and Refresh</span></a>
   </span>
 </span>
