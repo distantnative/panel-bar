c57e4c535638f3a39d568a211ed5cd3d9a6ae837
Nico Hoffmann <github@ohnehin.com>
2015-10-16 23:54:27 +0200

    CSS code cleaning

--- templates/controls.php
+++ templates/controls.php
@@ -1,11 +1,11 @@
 <div class="panelBar-controls" id="panelBar_controls">
   <div class="panelBar-controls__position" id="panelBar_position">
-    <i class="fa fa-arrow-circle-up panelBar-controls__position--top" title="To top (Alt + up)"></i>
-    <i class="fa fa-arrow-circle-down panelBar-controls__position--bottom" title="To bottom (Alt + down)"></i>
+    <i class="panelBar-controls__position--top fa fa-arrow-circle-up" title="To top (Alt + up)"></i>
+    <i class="panelBar-controls__position--bottom fa fa-arrow-circle-down" title="To bottom (Alt + down)"></i>
   </div>
   <div class="panelBar-controls__visibility" id="panelBar_visibility">
-    <i class="fa fa-times-circle panelBar-controls__visibility--visible" title="Hide (Alt + X)"></i>
-    <i class="fa fa-plus-circle panelBar-controls__visibility--hidden" title="Show (Alt + X)"></i>
-    <i class="fa fa-circle panelBar-controls__visibility--bg"></i>
+    <i class="panelBar-controls__visibility--visible fa fa-times-circle" title="Hide (Alt + X)"></i>
+    <i class="panelBar-controls__visibility--invisible fa fa-plus-circle" title="Show (Alt + X)"></i>
+    <i class="panelBar-controls__visibility--bg fa fa-circle"></i>
   </div>
 </div>
