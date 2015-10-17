229ca31c8a0848ce718c4a67666c9bd1181a4991
Nico Hoffmann <github@ohnehin.com>
2015-10-17 14:22:45 +0200

    State JS: optimized code

--- assets/js/components/localstorage.js
+++ assets/js/components/localstorage.js
@@ -3,29 +3,25 @@ var panelBarState = function() {
 
   var self = this;
 
-  this.validTime = 24 * 60 * 60 * 1000;
-
-  self.init = function() {
-    if(self.support()) {
-      if(self.expired()) {
+  this.init = function() {
+      if(Date.now() > localStorage.getItem('panelBar.expires')) {
         self.reset();
       } else {
         self.restore();
       }
       self.save();
       panelBar.controls.addEventListener('click', self.save);
-    }
   };
 
   this.save = function() {
-    localStorage.setItem('panelBar.expires',  Date.now() + self.validTime);
+    localStorage.setItem('panelBar.expires',  Date.now() + (24 * 60 * 60 * 1000));
     localStorage.setItem('panelBar.position', panelBar.position);
-    localStorage.setItem('panelBar.visible',  panelBar.visible);
+    localStorage.setItem('panelBar.visible',  panelBar.visible ? 'show' : 'hide');
   };
 
   this.restore = function() {
-    self.setPosition();
-    self.setVisibility();
+    panelBar[localStorage.getItem('panelBar.position')]();
+    panelBar[localStorage.getItem('panelBar.visible')]();
   };
 
   this.reset = function() {
@@ -33,28 +29,6 @@ var panelBarState = function() {
     localStorage.removeItem('panelBar.position');
     localStorage.removeItem('panelBar.visibile');
   };
-
-  this.setPosition = function() {
-    if(localStorage.getItem('panelBar.position') === 'top') {
-      panelBar.top();
-    } else {
-      panelBar.bottom();
-    }
-  };
-
-  this.setVisibility = function() {
-    if(localStorage.getItem('panelBar.visible') === 'true') {
-      panelBar.show();
-    } else {
-      panelBar.hide();
-    }
-  };
-
-
-  this.expired = function() {
-    return Date.now() > localStorage.getItem('panelBar.expires');
-  };
-
 
   this.support = function() {
     try {
@@ -69,8 +43,7 @@ var panelBarState = function() {
     }
   };
 
-  this.init();
+  if(this.support()) { this.init(); }
 };
-
 
 var pbState = new panelBarState();
